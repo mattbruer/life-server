6565b9ee966c9c1a2d4a0bb73a3008972fd705e6
Matt Bruer <mbruer@gmail.com>
2023-06-07 18:27:39 -0500

    get rid of comments

--- db/index.js
+++ db/index.js
@@ -1,12 +1,8 @@
 const db = require("./db");
 
 const Test = require("./models/Test");
-// const Song = require('./models/Song');
 
 //associations could go here!
-
-// User.hasMany(Song);
-// Song.belongsTo(User);
 
 module.exports = {
   db,
